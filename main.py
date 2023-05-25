bad4379ec4fef2992909b03da949beb78af63251
Juan Ferraro <juansebastianferraro@gmail.com>
2023-05-25 10:48:02 -0500

    Manejo de GET y creacion de json para películas

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.title = "Mi aplicacion con FastAPI"
+app.version = "0.0.1"
+
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
+    }
+]
 
 """ End Point """
-@app.get('/') 
+@app.get('/', tags=['Home']) 
 def message():
-    return "Hola Mundo!"
+    return HTMLResponse('<h1>Hola mundo</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
